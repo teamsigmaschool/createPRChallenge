72e2d49d101e43b7e1b318e91b9d9e33da5fe73c
teamsigmaschool <152258968+teamsigmaschool@users.noreply.github.com>
2023-11-28 08:14:57 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import SigmaPage from './pages/SigmaPage'
+
+export default function App() {
+  return (
+    <main>
+      <SigmaPage />      
+      <SigmaPage />
+    </main>
+  )
+}
